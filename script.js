a2535cfb4e9a034e277d88e5320b7277398f62cc
Sebas <sebastianquiudeloscocos@gmail.com>
2025-03-24 07:35:00 -0500

    Initial commit with large files using Git LFS

---
+++ script.js
@@ -0,0 +1,5 @@
+document.querySelectorAll('.download-btn').forEach(button => {
+    button.addEventListener('click', function() {
+        alert('¡Gracias por descargar! El archivo comenzará a descargarse ahora.');
+    });
+});
