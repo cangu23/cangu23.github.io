751bf54a514e69bd84968e0e8b2103cc5f97154e
CanguVT <162073871+cangu23@users.noreply.github.com>
2025-04-14 19:08:31 -0500

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-document.querySelectorAll('.download-btn').forEach(button => {
-    button.addEventListener('click', function() {
-        alert('¡Gracias por descargar! El archivo comenzará a descargarse ahora.');
-    });
-});
